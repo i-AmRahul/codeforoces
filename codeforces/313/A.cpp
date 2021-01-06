dfd71a5ab1af3ed6cdbd30b1c8f40d89e947fb11
dexter rayne <yrahul.14ap@gmail.com>
2021-01-06 18:14:00 +0530

    Add solution for problem `A - Ilya and Bank Account` Link: https://codeforces.com/contest/313/problem/A Tags: implementation, number theory, *900 Ref: https://codeforces.com/contest/313/submission/103540867

---
+++ codeforces/313/A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isVowel(char c)
+{
+    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' | c == 'y')
+    {
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+
+
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    if(n < 0)
+    {
+        n = max((n/10), ((n/100)*10) + (n%10));
+    }
+
+    cout << n << endl;
+    
+}
