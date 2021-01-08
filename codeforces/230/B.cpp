d80df3c4d6794936f1d23b194295db909c841573
dexter rayne <yrahul.14ap@gmail.com>
2021-01-08 22:35:00 +0530

    Add solution for problem `B - T-primes` Link: https://codeforces.com/contest/230/problem/B Tags: binary search, implementation, math, number theory, *1300 Ref: https://codeforces.com/contest/230/submission/103811172

---
+++ codeforces/230/B.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+/* bool isPrime(long long n)
+{
+    if(n == 1){
+        return false;
+    }
+    else{
+        for(long long i = 2; i*i <= n; i++)
+        {
+            if(n % i == 0)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+} */
+
+bool isPerfectSquare(long double n) 
+{
+  long double sr = sqrt(n); 
+  
+  return ((sr - floor(sr)) == 0); 
+} 
+
+bool isPrime[1000001];
+
+void sieve()
+{
+    //true means composite number.
+    long long N = 1000000;
+
+    isPrime[0] = true;
+    isPrime[1] = true;
+
+    for(int i = 2; i*i <= N; i++)
+    {
+        if(!isPrime[i]){
+            for(int j = i*i; j <= N; j+=i)
+            {
+                isPrime[j] = true;
+            }
+        }
+    }
+}
+
+ 
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+ 
+ 
+ 
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector<long long> v(n);
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    sieve();
+
+    for(long long x: v)
+    {
+        long double p = x;
+        if(x == 1 || x == 2)
+        {
+            cout << "NO" << endl;
+        }
+        else if(isPerfectSquare(p))
+        {
+            if(!isPrime[int(sqrt(x))]){
+                cout << "YES" << endl;
+            }
+            else
+            {
+                cout << "NO" << endl;
+            }
+            
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+        
+    }
+}
